f725838c04bf58a9d1bca7bc886a106ca2018a37
Brendan Ryan <brendan@bridge.xyz>
2026-02-03 16:06:13 -0800

    feat: add MODERATO_CHAIN_ID for testnet

--- src/mpay/methods/tempo/_defaults.py
+++ src/mpay/methods/tempo/_defaults.py
@@ -1,4 +1,8 @@
-"""Shared defaults for Tempo payment method (mainnet)."""
+"""Shared defaults for Tempo payment method."""
 
+# Tempo Moderato testnet
+MODERATO_CHAIN_ID = 42431
+
+# Tempo mainnet
 CHAIN_ID = 4217
 RPC_URL = "https://rpc.tempo.xyz"
